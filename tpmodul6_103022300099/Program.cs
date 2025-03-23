bc3704e76196b4c2c6aec4a7efc6a0ff8f93c7df
LENOVO <LENOVO@CELLAAAA>
2025-03-23 20:28:40 +0700

    Add project files.

---
+++ tpmodul6_103022300099/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        Random random = new Random();
+        this.id = random.Next(10000, 99999);
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        playCount += count;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine($"ID: {id}\nTitle: {title}\nPlay Count: {playCount}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – Sheila Nabilla Chantika Yudho");
+        video.IncreasePlayCount(100);
+        video.PrintVideoDetails();
+    }
+}
+
